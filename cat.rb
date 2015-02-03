e6301cea89ae58f979181ebaf64907a46e1670ae
richmolj <richmolj@gmail.com>
2015-02-03 18:12:19 -0500

    associations

--- cat.rb
+++ cat.rb
@@ -7,11 +7,15 @@ class Cat
     attrs.each_pair { |k,v| send("#{k}=", v) }
   end
 
+  def owner
+    OWNERS.find { |p| p.cat_name == @name }
+  end
+
   def meow
     if fat?
-      puts "CHEEZBURGR"
+      puts "Give me a CHEEZBURGR, #{owner.name}!"
     else
-      puts "Meow!"
+      puts "Meow, #{owner.name}!"
     end
   end
 
@@ -19,6 +23,19 @@ class Cat
     @weight > 25
   end
 end
+
+class Owner
+  attr_accessor :name
+  attr_accessor :cat_name
+
+  def initialize(attrs)
+    attrs.each_pair { |k,v| send("#{k}=", v) }
+  end
+end
+
+OWNERS = []
+OWNERS << Owner.new(name: 'John Doe',     cat_name: 'Felix')
+OWNERS << Owner.new(name: 'Jon Arbuckle', cat_name: 'Garfield')
 
 felix    = Cat.new(name: 'Felix',    color: 'black',  weight: 10)
 garfield = Cat.new(name: 'Garfield', color: 'orange', weight: 50)
